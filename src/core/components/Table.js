71bc7f53fbd734aef72d1615258c2bf8a9525d8d
Natalia Grishina <agrishinstudio@yandex.ru>
2024-06-15 23:53:57 +0300

    finish OOP

--- src/core/components/Table.js
+++ src/core/components/Table.js
@@ -1,33 +1,9 @@
 import { ExcelComponent } from "../ExcelComponent";
+import { createTable } from "./utils";
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
   toHTML() {
-    return `
-    <div class="table-row">
-            <div class="tabel-column-empty"></div>
-            <div class="tabel-column-count">A</div>
-            <div class="tabel-column-count">B</div>
-            <div class="tabel-column-count">C</div>
-          </div>
-          <div class="table-row-data">
-            <div class="tabel-row-count">1</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data 1A</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data 1B</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data 1C</div>
-          </div>
-          <div class="table-row-data">
-            <div class="tabel-row-count">2</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data 2A</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data 2B</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data 2C</div>
-          </div>
-          <div class="table-row-data">
-            <div class="tabel-row-count">3</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data</div>
-            <div class="tabel-column" contenteditable spellcheck="true">Data</div>
-          </div>
-    `
+    return createTable(20)
   }
 }
