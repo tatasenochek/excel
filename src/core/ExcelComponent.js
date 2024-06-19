f6061f604a65cbe2f5b40dde89f03eefe90ade19
Natalia Grishina <agrishinstudio@yandex.ru>
2024-06-19 13:24:55 +0300

    finish table

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,7 +5,9 @@ export class ExcelComponent extends Events {
     super($root, options.listners)
 
     this.name = options.name || ''
-
+    this.emitter = options.emitter
+    this.unsubscribers = []
+    
     this.prepare()
   }
 
@@ -15,11 +17,21 @@ export class ExcelComponent extends Events {
     return ''
   }
 
+  $emit(event, ...args) {
+    this.emitter.emit(event, ...args)
+  }
+
+  $on(event, fn) {
+    const unsub = this.emitter.subscribe(event, fn)
+    this.unsubscribers.push(unsub)
+  }
+
   init() {
     this.addEventListener()
   }
 
   destroy() {
     this.removeEventListener()
+    this.unsubscribers.forEach(unsub => unsub())
   }
 }
--- src/core/components/Table.js
+++ src/core/components/Table.js
@@ -6,9 +6,11 @@ import { createTable, isCell, shouldResize, tableResizeHendler, matrix, nextSele
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
-  constructor($root) {
+  constructor($root, options) {
     super($root, {
-      listners: ['mousedown', 'keydown']
+      name: 'Table',
+      listners: ['mousedown', 'keydown', 'input'],
+      ...options
     })
   }
 
@@ -23,8 +25,20 @@ export class Table extends ExcelComponent {
   init() {
     super.init()
 
-    const $cell = this.$root.find('[data-id="0:0"]')
+    this.selectCell(this.$root.find('[data-id="0:0"]'))
+
+    this.$on('formula:input', text => {
+      this.selection.current.text(text)
+    })
+
+    this.$on('formula:done', () => {
+      this.selection.current.focus()
+    })
+  }
+
+  selectCell($cell) {
     this.selection.select($cell)
+    this.$emit('table:select', $cell)
   }
 
   onMousedown(event) {
@@ -56,8 +70,12 @@ export class Table extends ExcelComponent {
 
       const id = this.selection.current.id(true)
       const $next = this.$root.find(nextSelector(key, id))
-      this.selection.select($next)
+      this.selectCell($next)
     }
+  }
+
+  onInput(event) {
+    this.$emit('table:input', $(event.target))
   }
 }
 
